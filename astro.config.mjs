e8cc18ebf85dfb4dae45dca6327b9b3e22ecad04
Donovan Crader <dcraderdev@gmail.com>
2026-05-17 17:06:30 -0700

    add sitemap + robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fieldletter.dcrader.dev',
   output: 'static',
   compressHTML: true,
   prefetch: { prefetchAll: true, defaultStrategy: 'viewport' },
-  integrations: [tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
